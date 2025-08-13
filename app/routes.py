446ab1a2f938b28ecedaa7eef893c1d417881a71
jon <lil.elma132@gmail.com>
2025-08-13 22:39:34 +0800

    Progress 3 - Database

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
